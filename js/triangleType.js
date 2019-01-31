d92700f4f3a1bd93a185fb431813f75153fd0458
Liv Stubbe Teglbjærg <livst89@users.noreply.github.com>
2019-01-31 17:46:13 +0100

    Update triangleType.js
    Moved the call of isValidTriangle() to new "main" function checkTriangle(), to separate the function calls.
    Added a simple function to determine triangle type, frindTriangleType().

--- js/triangleType.js
+++ js/triangleType.js
@@ -1,12 +1,17 @@
 let a = 0, b = 0, c = 0;
 
+function checkTriangle() {
+  getFormInput();
+  //isValidTriangle();
+  if (isValidTriangle()) {
+    findTriangleType();
+  };
+};
+
 function getFormInput() {
   a = parseInt(document.getElementById("sideA").value);
   b = parseInt(document.getElementById("sideB").value);
   c = parseInt(document.getElementById("sideC").value);
-
-  console.log(a,b,c);
-  isValidTriangle();
 };
 
 function isValidTriangle() {
@@ -14,7 +19,6 @@ function isValidTriangle() {
     console.log("Invalid: 0 or negative values.");
     return false;
   };
-  console.log(a+b, a+c, b+c);
   if (a + b > c && a + c > b && b + c > a) {
     console.log("Valid: Side lenghts form a valid triangle.");
     return true;
@@ -22,4 +26,15 @@ function isValidTriangle() {
     console.log("Invalid: Side lengths does not form a valid triangle.");
     return false;
   };
+};
+
+function findTriangleType() {
+  if (a == b && a == c) {
+    console.log("Equilateral");
+  } else if (a == b || a == c || b == c) {
+    console.log("Isosceles");
+  }
+  else {
+    console.log("Scalene");
+  }
 };
