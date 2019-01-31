f7f52a5576e329e68744164493c5eeb1bd9cb088
Liv Stubbe Teglbjærg <livst89@users.noreply.github.com>
2019-01-31 15:25:50 +0100

    Create triangleType.js
    Instantiating variables a, b and c. Using getFormInput() to get the values from the form input fields by element ID.
    Console log to check that values are correctly assigned to variables.

---
+++ js/triangleType.js
@@ -0,0 +1,9 @@
+let a = 0, b = 0, c = 0;
+
+function getFormInput() {
+  a = document.getElementById("sideA").value;
+  b = document.getElementById("sideB").value;
+  c = document.getElementById("sideC").value;
+
+  console.log(a,b,c);
+};
