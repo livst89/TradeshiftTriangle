cb181a7400004d994b71654ce9f283927b25c2bc
Liv Stubbe Teglbjærg <livst89@users.noreply.github.com>
2019-01-31 16:59:40 +0100

    Update triangleType.js
    Ran into trouble with the side length calculations due to not parsing form inputs to integers. Fixed with parseInt().
    isValidTriangle should now evaluate correctly whether the values input will form a valid triangle.

--- js/triangleType.js
+++ js/triangleType.js
@@ -1,9 +1,25 @@
 let a = 0, b = 0, c = 0;
 
 function getFormInput() {
-  a = document.getElementById("sideA").value;
-  b = document.getElementById("sideB").value;
-  c = document.getElementById("sideC").value;
+  a = parseInt(document.getElementById("sideA").value);
+  b = parseInt(document.getElementById("sideB").value);
+  c = parseInt(document.getElementById("sideC").value);
 
   console.log(a,b,c);
+  isValidTriangle();
+};
+
+function isValidTriangle() {
+  if (a <= 0 || b <= 0 || c <= 0) {
+    console.log("Invalid: 0 or negative values.");
+    return false;
+  };
+  console.log(a+b, a+c, b+c);
+  if (a + b > c && a + c > b && b + c > a) {
+    console.log("Valid: Side lenghts form a valid triangle.");
+    return true;
+  } else {
+    console.log("Invalid: Side lengths does not form a valid triangle.");
+    return false;
+  };
 };
